03a2a88221bb011b045705e4475fdbfcf00e984e
Andrea <ada@ada-desktop.(none)>
2009-07-15 19:34:05 +0200

    added italian inflections

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -8,3 +8,14 @@
 #   inflect.irregular 'person', 'people'
 #   inflect.uncountable %w( fish sheep )
 # end
+
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.plural /^(.*?)a$/i, '\1e'
+  inflect.plural /^(.*?)o$/i, '\1i'
+  inflect.plural /^(.*?)e$/i, '\1i'
+
+  inflect.singular /^(.*?)e$/i, '\1a'
+  inflect.singular /^(.*?)i$/i, '\1o'
+  inflect.singular /^(.*?)i$/i, '\1e'
+end
+
